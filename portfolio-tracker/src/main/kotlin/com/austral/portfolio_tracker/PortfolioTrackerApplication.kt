51af3213f96e5175bc28d3b1a05f2c99b5dda9ec
FranManfredi <85314753+FranManfredi@users.noreply.github.com>
2026-05-03 22:11:00 -0300

    initialize Spring Boot application with Gradle setup and basic configuration

---
+++ portfolio-tracker/src/main/kotlin/com/austral/portfolio_tracker/PortfolioTrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.austral.portfolio_tracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortfolioTrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication<PortfolioTrackerApplication>(*args)
+}
