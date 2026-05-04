ba7f2f7319d1f13354b2b009ccf6d571d831834f
FranManfredi <85314753+FranManfredi@users.noreply.github.com>
2026-05-04 00:19:34 -0300

    configure PostgreSQL datasource and add Docker support for portfolio tracker

--- portfolio-tracker/src/main/kotlin/com/austral/portfolio_tracker/PortfolioTrackerApplication.kt
+++ portfolio-tracker/src/main/kotlin/com/austral/portfolio_tracker/PortfolioTrackerApplication.kt
@@ -2,9 +2,16 @@ package com.austral.portfolio_tracker
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.TimeZone
 
 @SpringBootApplication
-class PortfolioTrackerApplication
+class PortfolioTrackerApplication {
+	companion object {
+		init {
+			TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+		}
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<PortfolioTrackerApplication>(*args)
