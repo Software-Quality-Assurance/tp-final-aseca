d0d80b008d2dab4783d0ec2f67c92c42b39ac924
carlix-27 <caacuna@mail.austral.edu.ar>
2026-05-06 17:23:16 -0300

    feat: add test for user dataclass.

--- portfolio-tracker/src/main/kotlin/com/austral/portfolio_tracker/entity/User.kt
+++ portfolio-tracker/src/main/kotlin/com/austral/portfolio_tracker/entity/User.kt
@@ -1,5 +1,30 @@
 package com.austral.portfolio_tracker.entity
 
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.validation.constraints.NotBlank
+import java.util.UUID
 
-data class User(val username: String, val password: String)
-
+@Entity
+data class User(
+    @Id
+    @NotBlank
+    val id: String = UUID.randomUUID().toString(),
+    @NotBlank
+    val name: String,
+    @NotBlank
+    val lastName: String,
+    @NotBlank
+    val email: String,
+    @NotBlank
+    val password: String
+)
+{
+    init {
+        require(id.isNotBlank()) { "Id must not be blank" }
+        require(name.isNotBlank()) { "Name must not be blank" }
+        require(lastName.isNotBlank()) { "Surname must not be blank" }
+        require(email.isNotBlank()) { "Email must not be blank" }
+        require(password.isNotBlank()) { "Password must not be blank" }
+    }
+}
--- portfolio-tracker/src/test/kotlin/com/austral/portfolio_tracker/user/UserTests.kt
+++ portfolio-tracker/src/test/kotlin/com/austral/portfolio_tracker/user/UserTests.kt
@@ -1,5 +1,8 @@
 package com.austral.portfolio_tracker.user
 
+import com.austral.portfolio_tracker.entity.User
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.assertNotNull
 import kotlin.test.Test
 
 class UserTests {
@@ -7,12 +10,21 @@ class UserTests {
     // Create user
     @Test
     fun `001 should create a new user`() {
-
+        val user = User("1", "John", "Doe", "johndoe@gmail.com", "mysecretpassword")
+        assertNotNull(user)
     }
 
-    // Almacenar el usuario creado
+    @Test
+    fun `002 new user must have id`() {
+        val user = User("1", "John", "Doe", "johndoe@gmail.com", "mysecretpassword")
+        assertNotNull(user.id)
+    }
+
+    @Test
+    fun `003 new user must not have an empty field and should throw an exception`() {
+        assertThrows(IllegalArgumentException::class.java) {  User(" ", " ", " ", " ", " ") }
+    }
 
 
-    // Endpoint - POST
 
 }
