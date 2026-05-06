a49ea3701564db5daae90e1b7d37160903afe590
carlix-27 <caacuna@mail.austral.edu.ar>
2026-05-06 17:45:41 -0300

    feat: add test for user email and id field

--- portfolio-tracker/src/main/kotlin/com/austral/portfolio_tracker/entity/User.kt
+++ portfolio-tracker/src/main/kotlin/com/austral/portfolio_tracker/entity/User.kt
@@ -2,8 +2,11 @@ package com.austral.portfolio_tracker.entity
 
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 import java.util.UUID
+
+private val emailRegex = Regex("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")
 
 @Entity
 data class User(
@@ -14,6 +17,7 @@ data class User(
     val name: String,
     @NotBlank
     val lastName: String,
+    @Email
     @NotBlank
     val email: String,
     @NotBlank
@@ -26,5 +30,6 @@ data class User(
         require(lastName.isNotBlank()) { "Surname must not be blank" }
         require(email.isNotBlank()) { "Email must not be blank" }
         require(password.isNotBlank()) { "Password must not be blank" }
+        require(emailRegex.matches(email)) { "Email must be valid" }
     }
 }
--- portfolio-tracker/src/test/kotlin/com/austral/portfolio_tracker/user/UserTests.kt
+++ portfolio-tracker/src/test/kotlin/com/austral/portfolio_tracker/user/UserTests.kt
@@ -1,7 +1,9 @@
 package com.austral.portfolio_tracker.user
 
 import com.austral.portfolio_tracker.entity.User
+import jakarta.persistence.Entity
 import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertNotNull
 import kotlin.test.Test
 
@@ -21,9 +23,37 @@ class UserTests {
     }
 
     @Test
-    fun `003 new user must not have an empty field and should throw an exception`() {
+    fun `003 user must be a JPA entity`() {
+        val entityAnnotation = User::class.java.getAnnotation(Entity::class.java)
+        assertNotNull(entityAnnotation)
+    }
+
+    @Test
+    fun `004 new user must not have an empty field and should throw an exception`() {
         assertThrows(IllegalArgumentException::class.java) {  User(" ", " ", " ", " ", " ") }
     }
+
+    // Each specified field
+
+    // id
+    @Test
+    fun `005 id must be generated automatically`() {
+        val user = User(name = "John", lastName = "Doe", email = "johndoe@gmail.com", password = "mysecretpassword")
+        assertNotNull(user.id)
+    }
+
+    // email
+    @Test
+    fun `006 a invalid email field should not throw an exception`() {
+        assertThrows(IllegalArgumentException::class.java) {  User(name = "John", lastName = "Doe", email = "johndoe", password = "mysecretpassword") }
+    }
+
+    @Test
+    fun `007 a valid email field should not throw an exception`() {
+        assertDoesNotThrow { User(name = "John", lastName = "Doe", email = "johndoe@gmail.com", password = "mysecretpassword") }
+    }
+
+
 
 
 
