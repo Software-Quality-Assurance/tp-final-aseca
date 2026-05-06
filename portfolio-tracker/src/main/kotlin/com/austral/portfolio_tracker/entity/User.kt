7e394e067425ddcd76c666c1a3ced126c9867e2c
carlix-27 <caacuna@mail.austral.edu.ar>
2026-05-06 16:49:25 -0300

    feat: refactoring

--- portfolio-tracker/src/main/kotlin/com/austral/portfolio_tracker/entity/User.kt
+++ portfolio-tracker/src/main/kotlin/com/austral/portfolio_tracker/entity/User.kt
@@ -1,36 +1,5 @@
 package com.austral.portfolio_tracker.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.Table
-import jakarta.validation.constraints.Email
-import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.Size
-import java.util.UUID
 
-@Entity
-@Table(name = "users")
-data class User(
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    val id: UUID = UUID.randomUUID(),
-    @field:NotBlank(message = "First name cannot be empty")
-    @field:Size(min = 2, max = 100, message = "First name must be between 2 and 100 characters")
-    @Column(nullable = false)
-    val firstName: String,
-    @field:NotBlank(message = "Last name cannot be empty")
-    @field:Size(min = 2, max = 100, message = "Last name must be between 2 and 100 characters")
-    @Column(nullable = false)
-    val lastName: String,
-    @field:NotBlank(message = "Email cannot be empty")
-    @field:Email(message = "Email must be valid")
-    @Column(nullable = false, unique = true)
-    val email: String,
-    @field:NotBlank(message = "Password cannot be empty")
-    @field:Size(min = 8, message = "Password must be at least 8 characters")
-    @Column(nullable = false)
-    val passwordHash: String,
-)
+data class User(val username: String, val password: String)
+
---
+++ portfolio-tracker/src/test/kotlin/com/austral/portfolio_tracker/user/UserTests.kt
@@ -0,0 +1,18 @@
+package com.austral.portfolio_tracker.user
+
+import kotlin.test.Test
+
+class UserTests {
+
+    // Create user
+    @Test
+    fun `001 should create a new user`() {
+
+    }
+
+    // Almacenar el usuario creado
+
+
+    // Endpoint - POST
+
+}
