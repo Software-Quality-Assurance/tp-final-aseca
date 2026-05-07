939e4be0ea25ed28cb8126eebef3d752d10ed426
FranManfredi <FManfredi@mail.austral.edu.ar>
2026-05-07 17:03:00 -0300

    feat: add generated value strategy for portfolio ID

--- portfolio-tracker/src/main/kotlin/com/austral/portfolio_tracker/entity/Portfolio.kt
+++ portfolio-tracker/src/main/kotlin/com/austral/portfolio_tracker/entity/Portfolio.kt
@@ -2,6 +2,8 @@ package com.austral.portfolio_tracker.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.OneToOne
@@ -16,6 +18,7 @@ data class Portfolio(
     @Id
     @NotBlank
     @Column(name = "id", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: String = UUID.randomUUID().toString(),
     @OneToOne
     @NotNull
